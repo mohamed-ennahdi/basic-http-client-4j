70d7824da1c513162b729f5b71177775b4bae229
Mohamed ENNAHDI EL IDRISSI <mohamed.ennahdi@gmail.com>
2025-08-15 14:52:17 +0000

    FakeStoreAPI test cases

---
+++ src/test/java/com/github/mohamedennahdi/basichttpclient4j/client/BasicHTTPClientPlatziFakeStoreAPITest.java
@@ -0,0 +1,93 @@
+package com.github.mohamedennahdi.basichttpclient4j.client;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.annotation.processing.Generated;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.mohamedennahdi.basichttpclient4j.client.utils.ClientUtils;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+@Generated(value = "org.junit-tools-1.1.0")
+public class BasicHTTPClientPlatziFakeStoreAPITest {
+	
+	public static final Logger LOGGER = LoggerFactory.getLogger(BasicHTTPClientPlatziFakeStoreAPITest.class);
+	String url = "https://api.escuelajs.co/api/v1/products";
+	
+	@Test
+	public void doGetTest() throws Exception {
+		BasicHTTTPClient testSubject;
+		CloseableHttpResponse actual;
+		
+		LOGGER.info("Calling {}", this.url);
+
+		// default test
+		testSubject = new BasicHTTTPClient.Builder()
+				.setUrl(url)
+				.build();
+		actual = testSubject.doGet();
+
+		assertEquals(actual.getStatusLine().getStatusCode(), 200);
+		/*
+		 *
+		 */
+		String json = EntityUtils.toString(actual.getEntity());
+		JsonArray jsonObject = new Gson().fromJson(json, JsonArray.class);
+		assertTrue(jsonObject.size() > 0);
+		String productId = jsonObject.get(0).getAsJsonObject().get("id").getAsString();
+		LOGGER.info("Calling {}", this.url + "/" + productId);
+
+		// default test
+		testSubject = new BasicHTTTPClient.Builder()
+				.setUrl(this.url + "/" + productId)
+				.build();
+		actual = testSubject.doGet();
+
+		assertEquals(actual.getStatusLine().getStatusCode(), 200);
+		/*
+		 *
+		 */
+		String expected = ClientUtils.readResource("product159.json");
+		json = EntityUtils.toString(actual.getEntity());
+		JsonObject actualJSON = new Gson().fromJson(json, JsonObject.class);
+		JsonObject expectedJSON = new Gson().fromJson(expected, JsonObject.class);
+		assertEquals(expectedJSON, actualJSON);
+
+	}
+
+	@Test
+	public void doPostTest() throws Exception {
+		BasicHTTTPClient testSubject;
+		CloseableHttpResponse actual;
+
+		String body = ClientUtils.readResource("newproduct.json");
+		body = body.replaceAll("New Product", UUID.randomUUID().toString());
+		
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json");
+		headers.put("Accept", "text/plain");
+
+		testSubject = new BasicHTTTPClient.Builder()
+				.setUrl(this.url)
+				.setHeaders(headers)
+				.setBody(body)
+				.build();
+
+		actual = testSubject.doPost();
+
+		assertEquals(201, actual.getStatusLine().getStatusCode());
+	}
+}
